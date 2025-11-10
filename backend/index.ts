df41f34e8ae84f2365eb3f9c21b8dc1d62886616
T1Angel4220 <aayuquina4220@uta.edu.ec>
2025-11-10 16:35:18 -0500

    fix:port

--- backend/index.ts
+++ backend/index.ts
@@ -8,7 +8,7 @@ const ProductsController = require('./src/controllers/productsController');
 // Cargar variables de entorno
 dotenv.config();
 
-const PORT = config.server.port;
+const PORT = process.env.PORT || config.server.port || 3001;
 
 // Función para inicializar el servidor
 const startServer = async () => {
